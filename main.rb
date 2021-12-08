17bb7872a1e5dc7ca09d05efa27524667b8a9558
pseudotsuga-fir <hepworta@oregonstate.edu>
2021-12-08 11:17:48 -0800

    fix formatting in main.rb. References: #1

--- main.rb
+++ main.rb
@@ -1,44 +1,19 @@
 require_relative 'game'
-  require_relative 'box'
-  require_relative 'die'
-
-
+require_relative 'box'
+require_relative 'die'
 
 NUM_T = 9
 N_DICE = 2
 N_SIDES = 6
-
 
 dice= (1..N_DICE).map { |i| Die.new(N_SIDES) }
 box= Box.new(NUM_T)
 
 game=Game.new(box, dice)
 
-
-
-
-
 puts Game::WELCOME_MESSAGE
- game.start!
+
+game.start!
 game.play until game.over?
 
-puts   game.results
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+puts game.results
